aa9608cff6f21554f1e327d765e1b879a236a233
Ilya Golovanov <iliya-golovanov@yandex.ru>
2019-05-27 23:46:50 +0300

    first commit

---
+++ init.sh
@@ -0,0 +1,4 @@
+# sudo ln -sf ~/code/stepik1/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx start
+# sudo /etc/init.d/nginx restart
